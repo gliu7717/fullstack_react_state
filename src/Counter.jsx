4511f5c6ac2a963964b171d29a07ca60334e9765
rayliu7717 <rayliu2004@gmail.com>
2026-01-20 19:51:57 -0500

    useState hook

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 const Counter = () => {
+  //console.log(useState(0));
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("black");
   // Normal variable (will NOT trigger re-render)
-  let count = 0;
+  //let count = 0;
 
   const increment = () => {
-    // Updating the value of count does not update UI
-    count++;
-    console.log("Count:", count);
+    const newCount = count + 1;
+    // Update state, trigger re-render
+    setCount(newCount);
+    // Change color based on count value
+    if (newCount % 2 === 0) {
+      setColor("blue");
+    } else {
+      setColor("red");
+    }
+    // Log the new value of count
+    console.log("Count:", newCount);
   };
   return (
     <div>
-      <h2>Counter: {count}</h2>
+      <h2 style={{ color }}>Counter: {count}</h2>
       <button onClick={increment}>Increment</button>
     </div>
   );
